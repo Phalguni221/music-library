648838362442777c3876c4958be9ff0c13d627f3
Sriya Bhamidipati <88012398+Phalguni221@users.noreply.github.com>
2022-02-02 18:17:34 -0500

    music_library

---
+++ src/components/SearchBar.js
@@ -0,0 +1,19 @@
+// Searchbar.js
+import { useState } from 'react'
+
+function SearchBar(props){
+    // We can comment out our searchTerm state variable as we are not using it!
+    // let [searchTerm, setSearchTerm] = useState('')
+
+    return (
+            <form>
+                     <input type="text" placeholder="Search Here"
+                    onChange={
+                        (e) => props.handleSearch(e, e.target.value)
+                    } />
+                     <input type="reset" id="reset" />
+            </form>
+    )
+}
+
+export default SearchBar
